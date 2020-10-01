234886d4234267a0da0f385c2b956f419ab2a613
ALPpedro <42361950+ALPpedro@users.noreply.github.com>
2020-10-01 01:04:19 -0300

    Initial commit

---
+++ components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title has-text-grey">
+          {{ title }}
+        </p>
+      </header>
+      <div class="card-content">
+        <p class="content has-text-grey">
+          {{ texto }}
+        </p>
+      </div>
+      <footer class="card-footer">
+        <div class="card-footer-item">
+          <span>
+            <p>Postado por esse corno: {{ autor }}</p>
+          </span>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    autor: {
+      type: String,
+      required: true
+    },
+    texto: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <div class="rows is-mobile">
+      <card
+        title="Fatec em crise"
+        texto="Todos os alunos da fatec ficaram chocados com a tarefa em cima da hora do sdsdff dfsdfsdf "
+        autor="Claudinei"
+      >
+      </card>
+      <card
+        title="Fatec em crise"
+        texto="Todos os alunos da fatec ficaram chocados com a tarefa em cima da hora do sdsdff dfsdfsdf "
+        autor="Claudinei"
+      >
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
